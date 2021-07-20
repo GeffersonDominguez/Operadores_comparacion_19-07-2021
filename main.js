f0501c947e4c7ef63a3644360c0beb0e1f3c7f56
GeffersonDominguez <geffersondominguez93@gmail.com>
2021-07-19 21:48:32 -0500

    Operadores de comparación 19-07-21

---
+++ main.js
@@ -0,0 +1,24 @@
+let a= 8;
+let b= 20;
+
+// Mayor que
+console.log(`El a =${a} es mayor que b = ${b} : ${a>b}`,`Es falso porque 8 no es mayor que 20`);
+
+// Mayor o igual que
+console.log(`El a =${a} es mayor o igual que b = ${b}`,(a>=b),`;Es falso porque 8 no es mayor o igual que 20`);
+
+// Menor que
+console.log(`El a =${a} es menor que b = ${b}`,(a<b),`;Es verdadero porque 8 es menor que 20`);
+
+// Menor o igual que
+console.log(`El a =${a} es menor o igual que b = ${b}`,(a<=b),`;Es verdadero porque 8 es menor o igual que 20`);
+
+//Igual que
+console.log(`El a =${a} es igual que b = ${b}`,(a==b), `;Es falso porque 8 no es igual que 20`);
+
+//Diferente que
+console.log(`El a =${a} es diferente que b = ${b}`,(a!=b),`;Es verdadero porque 8 es diferente de 20`);
+
+
+
+
